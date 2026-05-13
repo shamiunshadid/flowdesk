67ef578ed3e2c955d0c001bbee2f9449207593ee
Shamiun Shadid <shamiunshadid@gmail.com>
2026-05-13 22:10:57 +0600

    changes: removed default prisma and added custom db

--- backend/lib/auth.ts
+++ backend/lib/auth.ts
@@ -1,16 +1,9 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../src/generated/prisma/client";
-
-const prisma = new PrismaClient({
-  adapter: new PrismaPg({
-    connectionString: process.env.DATABASE_URL!,
-  }),
-});
+import { db } from "./db";
 
 export const auth = betterAuth({
-  database: prismaAdapter(prisma, {
+  database: prismaAdapter(db, {
     provider: "postgresql",
   }),
   trustedOrigins: ["http://localhost:3000", "http://localhost:8000"],
