f65713a5d8bebbfe92cb399862251ca9d98db28d
Shamiun Shadid <shamiunshadid@gmail.com>
2026-05-13 22:01:44 +0600

    removed the auto generated prisma.ts file and added this custom db file, where db is the source of truth.

---
+++ backend/lib/db.ts
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import { PrismaClient } from "../src/generated/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const adapter = new PrismaPg(pool);
+
+export const db = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
+}
