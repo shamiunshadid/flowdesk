ef86e4116a4ee33fc9b07f1e95531efe9fb87eb2
Shamiun Shadid <shamiunshadid@gmail.com>
2026-05-13 21:53:34 +0600

    feat: workspace routes created

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,29 @@
-console.log("Hello via Bun!");
+import express from "express";
+import env from "dotenv"
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+
+import workspaceRoutes from "./src/routes/v1/workspace.routes";
+
+const app = express();
+env.config()
+const port = 8000;
+
+app.use(express.json());
+
+// auth routes
+app.all("/api/auth/{*path}", toNodeHandler(auth));
+
+
+// workspace routes
+app.use("/api/workspace", workspaceRoutes);
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello from bunnn");
+});
+
+app.listen(port, () => {
+  console.log("server is running on port:", port);
+});
